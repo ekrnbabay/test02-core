5415de573c2edcc6921f32217d36b7123c085629
Raul Averianov <raul.averianov@dataart.com>
2019-08-08 13:44:30 +0300

    #install  .Net Core on the ubuntu

--- aspnetcoreapp/setup.sh
+++ aspnetcoreapp/setup.sh
@@ -1,4 +1,21 @@
-#ubuntu
+#install java if need 
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+sudo apt-get update
+sudo apt install npm -y
+
+#install  .Net Core on the ubuntu
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo add-apt-repository universe
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2 -y
+
+#web nginx
+sudo apt-get install nginx -y
+
+#Publish project ubuntu from local dir to /var/www/aspnetcoreapp
 sudo mkdir /var/www/aspnetcoreapp
 cp aspnetcoreapp.service /etc/systemd/system/aspnetcoreapp.service
 #sudo nano /etc/systemd/system/aspnetcoreapp.service
@@ -6,3 +23,6 @@ sudo dotnet publish -c Release -o /var/www/aspnetcoreapp
 sudo systemctl enable aspnetcoreapp.service
 sudo service aspnetcoreapp start
 # service --status-all
+
+#setup proxy nginx from port 5000
+...
